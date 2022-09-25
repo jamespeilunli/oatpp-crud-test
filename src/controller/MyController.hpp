1cbbecb11ec36bcb59ffdf01492aa98fb72e8e80
James Li <jamespeilunli@gmail.com>
2022-09-25 14:57:00 -0700

    add basic create endpoint

--- src/controller/MyController.hpp
+++ src/controller/MyController.hpp
@@ -1,6 +1,10 @@
 #ifndef MyController_hpp
 #define MyController_hpp
 
+#include <string>
+#include <iostream>
+#include <fstream>
+
 #include "dto/DTOs.hpp"
 
 #include "oatpp/web/server/api/ApiController.hpp"
@@ -31,7 +35,55 @@ public:
   }
   
   // TODO Insert Your endpoints here !!!
-  
+
+  // https://github.com/oatpp/oatpp/issues/20
+  ENDPOINT("GET", "isEven/*", getUserWithQueryParams,
+           REQUEST(std::shared_ptr<IncomingRequest>, request) // Map request object to endpoint method
+           ) {
+
+    /* get url 'tail' - everything that comes after '*' */
+    String tail = request->getPathTail(); // everything that goes after '*'
+
+    /* check tail for null */
+    OATPP_ASSERT_HTTP(tail, Status::CODE_400, "null query-params");
+
+    /* parse query params from tail */
+    auto queryParams = oatpp::network::Url::Parser::parseQueryParams(tail);
+
+    /* get your param by name */
+    std::string input = queryParams.get("input");
+
+    // act upon the input
+    std::string output = stoi(input) % 2 == 0 ? "true" : "false";
+
+    /* return result */
+    return createResponse(Status::CODE_200, "output=" + output);
+
+  }
+
+    ENDPOINT("GET", "/create", getUsers,
+           QUERIES(QueryParams, queryParams)) {
+
+           std::ofstream data_file;
+           data_file.open ("data.json");
+           data_file << "{";
+           bool first = true;
+           for (auto& param : queryParams.getAll()) {
+               int key = std::stoi(param.first.std_str());
+               int value = std::stoi(param.second.std_str());
+
+               if (first) {
+                   data_file << key << ":" << value;
+                   first = false;
+               } else {
+                   data_file << "," << key << ":" << value;
+               }
+           }
+           data_file << "}";
+           data_file.close();
+
+        return createResponse(Status::CODE_418, "Thirsty Vitus");
+    }
 };
 
 #include OATPP_CODEGEN_END(ApiController) //<-- End Codegen
